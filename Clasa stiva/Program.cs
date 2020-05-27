c5a1c01b7cee8329940a74350dfb3da8187b0574
Feri Czenk <feri.czenk@hotmail.com>
2020-05-27 15:44:40 +0300

    Add project files.

---
+++ Clasa stiva/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clasa_stiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stiva<string> StrStack = new Stiva<string>(5);
+
+            StrStack.Push("a");
+            StrStack.Push("b");
+            StrStack.Push("c");
+            StrStack.Push("d");
+            StrStack.Push("e");
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(StrStack.Pop());
+            }
+
+
+            Stiva<int> IntStack = new Stiva<int>(5);
+
+            for (int i = 0; i < 5; i++)
+            {
+                IntStack.Push(i);
+            }
+
+            IntStack.Clear();
+
+            Console.WriteLine(IntStack.Pop());
+        }
+    }
+}
---
+++ Clasa stiva/Stiva.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Clasa_stiva
+{
+    class Stiva<T>
+    {
+        private T[] data;
+        private int length = 0;
+        public Stiva()
+        {
+            data = new T[100];
+        }
+
+        public Stiva(int n)
+        {
+            data = new T[n];
+        }
+
+        public void Push(T newElement)
+        {
+            if (length < data.Length)
+            {
+                data[length++] = newElement;
+            }
+            else
+            {
+                throw new Exception("Stiva este plina");
+            }
+        }
+
+        public T Pop()
+        {
+            if (length != 0)
+            {
+                length--;
+                return data[length];
+            }
+            else
+            {
+                throw new Exception("Stiva este goala");
+            }
+        }
+
+        public void Clear()
+        {
+            data = new T[data.Length];
+            length = 0;
+        }
+    }
+}
